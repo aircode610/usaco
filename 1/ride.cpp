28d713adf170e50b183186c9231eaca01a2a52fb
aircode610 <aircode610@gmail.com>
2020-11-27 19:30:37 +0330

    solved problem 1 ;)

---
+++ 1/ride.cpp
@@ -0,0 +1,38 @@
+/*
+ID: your_id_here
+TASK: ride
+LANG: C++11                 
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int convert(string str){
+  int product = 1;
+  for (int i = 0; i < str.size(); i++){
+    product *= (int)str[i] - 64;
+  }
+
+  return product % 47;
+}
+
+int main() {
+  
+  ifstream fin("ride.in");
+  ofstream fout("ride.out");
+
+  string comet, group;
+  fin >> comet >> group;
+  
+  if (convert(comet) == convert(group)){
+    fout << "GO" << endl;
+  }
+  else {
+    fout << "STAY" << endl;
+  }
+  
+  return 0;
+  
+}
